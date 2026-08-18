f48de6886c9121baf6742df92507fba2246c404f
dylanhoang11 <dylan.hoang@walmart.com>
2026-08-18 10:34:35 -0500

    Manual Push 2

---
+++ scripts/_gen.py
@@ -0,0 +1,4 @@
+import os 
+ 
+exec(open('_auth_content.txt').read()) 
+print('Created all files')
---
+++ scripts/setup_gcp_auth.py
@@ -0,0 +1,5 @@
+import subprocess, sys, os  
+from pathlib import Path  
+  
+print('GCP Authentication Setup for Unloader System')  
+print('='*60) 
